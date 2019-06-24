89f9a6f38ea87e8c5082095637f96b6b2ea165ed
Akira Shinohara <k017c1067@it-neec.jp>
2019-06-24 09:40:15 +0900

    initial commit.

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'note.mojamoja.work'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
