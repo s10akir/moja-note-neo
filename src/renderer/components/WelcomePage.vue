d25c65a55e68f57167aff1eb4949f7729d9ae123
Akira Shinohara <k017c1067@it-neec.jp>
2019-07-16 15:15:35 +0900

    [fix] fix style for element-ui

--- src/renderer/components/WelcomePage.vue
+++ src/renderer/components/WelcomePage.vue
@@ -1,19 +1,17 @@
 <template>
-  <main class="hero is-fullheight">
-    <div class="hero-body">
-      <div class="container">
-        <img id="logo" src="~@/assets/logo.png" alt>
-  
-        <h1 class='title is-2'>Welcome to moja-note!</h1>
-        <p>この環境でmoja-noteを起動するのは初めてのようです。</p>
-        <p>{DBを設置したパス}にデータベースを作成しました。うっかり削除しないように。</p>
-  
-        <br>
-  
-        <router-link to="/main" class="button is-primary is-medium">Let's Start!</router-link>
-      </div>
-    </div>
-  </main>
+  <div>
+    <img id="logo" src="~@/assets/logo.png" alt>
+      
+    <h1>Welcome to moja-note!</h1>
+    <p>この環境でmoja-noteを起動するのは初めてのようです。</p>
+    <p>{DBを設置したパス}にデータベースを作成しました。うっかり削除しないように。</p>
+      
+    <br>
+      
+    <router-link to="/main">
+      <el-button type="success">Let's Start!</el-button>
+    </router-link>
+  </div>
 </template>
 
 <script>
